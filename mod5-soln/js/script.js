5dbaf706257e1646dc681aa0582606960f115cbe
LikhitaS <likhitaswt1999@gmail.com>
2020-05-13 08:55:16 +0530

    final solution

--- mod5-soln/js/script.js
+++ mod5-soln/js/script.js
@@ -104,6 +104,7 @@ function buildAndShowHomeHTML (categories) {
        var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
+
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -115,14 +116,14 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-       var homeHtmlToInsertIntoMainPage = insertProperty(html,randomCategoryShortName,chosenCategoryShortName);
+       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,'randomCategoryShortName',"'"+chosenCategoryShortName+"'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      innerHTML(html,homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
